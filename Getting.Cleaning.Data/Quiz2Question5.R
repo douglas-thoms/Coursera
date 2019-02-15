31244347604048987cb453aa5cd793b2d5cd8444
douglas-thoms <douglas.thoms@gmail.com>
2019-02-15 01:18:44 -0500

    2nd assignment complete

--- Getting.Cleaning.Data/Quiz2Question5.R
+++ Getting.Cleaning.Data/Quiz2Question5.R
@@ -1,15 +1,27 @@
 scrape_web <-function(){
+
+  
+  if(!file.exists("data")){
+    
+    dir.create("data")
+    
+  }
   
   url <- ("https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for")
   
-  download.file(url,destfile = "quiz2question5data.for")
+  download.file(url,destfile = "./data/quiz2question5data.for")
   
-  raw.data <- read.fortran("quiz2question5data.for") 
+  raw.data <- read.fortran(file = "./data/quiz2question5data.for",
+                           c("1X", "A9",
+                            "5X","F4.0","F4.0",
+                            "5X","F4.0","F4.0",
+                            "5X","F4.0","F4.0",
+                            "5X","F4.0","F4.0"),
+                            skip=4) 
   
   
-  json2 <- jsonlite::fromJSON(jsonlite::toJSON(raw.data))
   
   
-  return(json2)
+  return(sum(raw.data$V4))
   
 }
