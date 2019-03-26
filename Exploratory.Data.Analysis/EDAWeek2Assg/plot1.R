185ab2954ffd1f7bea772eba7f261b921f3b2e46
douglas-thoms <douglas.thoms@gmail.com>
2019-03-26 10:07:45 -0400

    first commit

---
+++ Exploratory.Data.Analysis/EDAWeek2Assg/plot1.R
@@ -0,0 +1,18 @@
+##----------------------------------------------------------------------------
+##----------------------------------------------------------------------------
+##
+##  File name:  plot1.R
+##  Date:       26Mar2019
+##
+##  Part of Course Project 2 for Exploratory data course
+##      
+##
+##----------------------------------------------------------------------------
+##----------------------------------------------------------------------------
+
+
+
+
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
