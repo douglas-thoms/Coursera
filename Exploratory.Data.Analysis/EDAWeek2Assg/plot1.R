1a65e589c530c41fd0b160af02f5bf7a69f26884
douglas-thoms <douglas.thoms@gmail.com>
2019-03-26 13:03:34 -0400

    plot 1 started

--- Exploratory.Data.Analysis/EDAWeek2Assg/plot1.R
+++ Exploratory.Data.Analysis/EDAWeek2Assg/plot1.R
@@ -10,9 +10,18 @@
 ##----------------------------------------------------------------------------
 ##----------------------------------------------------------------------------
 
-
+#Have total emissions from PM2.5 decreased in the United States from 
+#1999 to 2008? Using the base plotting system, make a plot showing 
+#the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 
 
 ## This first line will likely take a few seconds. Be patient!
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
+#NEI <- readRDS("summarySCC_PM25.rds")
+#SCC <- readRDS("Source_Classification_Code.rds")
+
+df <- lapply(NEI[,c("fips","SCC","Pollutant","type","year")], as.factor)
+df$Emissions <- NEI$Emissions
+df <- as.data.frame(df)
+
+boxplot(log10(df$Emission) ~ df$year)
+
