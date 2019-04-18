45aea2dafcd2b0191011c75375e7d3388a19c121
douglas-thoms <douglas.thoms@gmail.com>
2019-04-18 09:54:31 -0400

    started assignment 2, fixed typos assignment 1

---
+++ Johns.Hopkins.Data.Science.Specialization/Reproducible.Research/Assignment.2/Assignment.2.R
@@ -0,0 +1,34 @@
+##----------------------------------------------------------------------------
+##----------------------------------------------------------------------------
+##
+##  File name:  Assignment.2.R
+##  Date:       18Apr2019
+##
+##  Assignment 2 for Reproducible Research course
+##  
+##
+##----------------------------------------------------------------------------
+##----------------------------------------------------------------------------
+
+
+#Does the document have a title that briefly summarizes the data analysis?
+#1 -3 figures
+#Does the analysis start from the raw data file (i.e. the original .csv.bz2 file)?
+#Does the analysis address the question of which types of events are most harmful to population health?
+#Does the analysis address the question of which types of events have the greatest economic consequences?
+#Do all the results of the analysis (i.e. figures, tables, numerical summaries) appear to be reproducible?
+#Do the figure(s) have descriptive captions (i.e. there is a description near the figure of what is happening in the figure)?
+
+#QUESTIONS
+
+#Across the United States, which types of events (as indicated in the \color{red}{\verb|EVTYPE|}EVTYPE variable) are most harmful with respect to population health?
+#Across the United States, which types of events have the greatest economic consequences?
+
+#You must show all your code for the work in your analysis document. This may make the document a bit verbose, but that is okay. 
+#In general, you should ensure that \color{red}{\verb|echo = TRUE|}echo=TRUE for every code chunk (this is the default setting in knitr).
+
+#DOCUMENT TITLE
+
+#DATA PROCESSING
+
+#RESULTS
