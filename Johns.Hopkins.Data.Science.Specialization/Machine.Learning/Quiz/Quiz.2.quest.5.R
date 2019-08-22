0a5b2c303bf11c07d66960321029e1026632b620
douglas-thoms <douglas.thoms@gmail.com>
2019-08-22 09:41:18 -0400

    quiz 2

--- Johns.Hopkins.Data.Science.Specialization/Machine.Learning/Quiz/Quiz.2.quest.5.R
+++ Johns.Hopkins.Data.Science.Specialization/Machine.Learning/Quiz/Quiz.2.quest.5.R
@@ -8,7 +8,7 @@ training = adData[ inTrain,]
 testing = adData[-inTrain,]
 
 trainingIL <- training[,grep("^IL|diagnosis", names(training))]
-testingIL2 <- testing[c(1,grep("^IL.*",names(testing)))]
+testingIL <- testing[c(1,grep("^IL.*",names(testing)))]
 
 #Non-PCA model
 
@@ -18,23 +18,21 @@ testingIL2 <- testing[c(1,grep("^IL.*",names(testing)))]
 
 #PCA Model
 #determine PCA to use to reach .8
-procTrain <- preProcess(trainingIL[,-13], method="pca", thres = 0.8)
+pre.proc <- preProcess(trainingIL[,-13], method="pca", thres = 0.8)
 
 #calculate PCA data set
-trainPC <- predict(procTrain,trainingIL[,-13])
-testPC <- predict(procTrain,testingIL[,-13])
+trainPC <- predict(pre.proc,trainingIL[,-13])
+testPC <- predict(pre.proc,testingIL[,-13])
 
 #apply to model
 
 model <- train(diagnosis ~ ., data = trainingIL, method = "glm")
-predict_model <- predict(model, newdata = testingIL)
 results <- confusionMatrix(predict(model,testingIL),testingIL$diagnosis)
 
 
 modelFitPC <- train(diagnosis ~ ., method = "glm", data=trainPC)
 resultsPC <- confusionMatrix(testingIL$diagnosis,predict(modelFitPC,testPC))
 
-#procTest <- preProcess(trainingIL[,-1], method="pca", pcaComp = 7)
 
 
 
