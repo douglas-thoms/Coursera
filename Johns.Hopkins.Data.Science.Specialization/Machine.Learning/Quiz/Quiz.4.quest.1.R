f264c887bfc9a4367a7f3b852a3a197061c67029
douglas-thoms <douglas.thoms@gmail.com>
2019-09-03 16:33:39 -0400

    quiz 4

---
+++ Johns.Hopkins.Data.Science.Specialization/Machine.Learning/Quiz/Quiz.4.quest.1.R
@@ -0,0 +1,28 @@
+library(ElemStatLearn)
+
+data(vowel.train)
+vowel.train$y <- as.factor(vowel.train$y)
+data(vowel.test)
+vowel.test$y <- as.factor(vowel.test$y)
+
+set.seed(33833)
+
+library(caret)
+
+# Set the variable y to be a factor variable in both the training and test set. 
+# Then set the seed to 33833. Fit (1) a random forest predictor relating 
+# the factor variable y to the remaining variables and (2) a boosted predictor 
+# using the "gbm" method. Fit these both with the 
+# train() command in the caret package.
+
+fit1 <- train(y ~., method="rf", data = vowel.train)
+pred1 <- predict(fit1,vowel.test)
+result1 <- confusionMatrix(pred1,vowel.test$y)
+
+
+fit2 <- train(y ~., method="gbm", data = vowel.train)
+pred2 <- predict(fit2,vowel.test)
+result2 <- confusionMatrix(pred2,vowel.test$y)
+
+a <- qplot(pred1,pred2,colour=y,data=vowel.test)
+print(a)
