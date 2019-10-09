c885fc987cf8f6790253ae54fb58a74bd6c1893c
douglas-thoms <douglas.thoms@gmail.com>
2019-10-09 10:54:59 -0400

    Assignment 4

--- Johns.Hopkins.Data.Science.Specialization/Data.Products/Assignment.4/server.R
+++ Johns.Hopkins.Data.Science.Specialization/Data.Products/Assignment.4/server.R
@@ -1,8 +1,9 @@
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output,session) {
         
         
         options(cancensus.api_key = "CensusMapper_7dd2122753cfb78fec890b61d909bdc4")
+        options(cancensus.cache_path = getwd())
         
         census_variables <- c("v_CA16_5051","v_CA16_5054","v_CA16_5057","v_CA16_5060","v_CA16_5063",
                               "v_CA16_5066","v_CA16_5069","v_CA16_5072","v_CA16_5075",
@@ -12,12 +13,36 @@ server <- function(input, output) {
         census_data <- get_census(dataset='CA16', regions=list(C="01"), labels = "short", 
                                   vectors=census_variables, level='PR', geo_format='sf')
         
-        output$distPlot <- renderPlot({
-                # generate bins based on input$bins from ui.R
-                x    <- faithful[, 2] 
-                bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        output$map <- renderLeaflet({
+                #base map on select input
                 
-                # draw the histogram with the specified number of bins
-                hist(x, breaks = bins, col = 'darkgray', border = 'white')
+                #calculate population that have this degree as their highest education
+                
+                #does not accept input variable but accepts if put regular column in
+                
+                census_data <- census_data %>%
+                        mutate(map_data = (input$variable/Population)*100)
+                
+                pal <- colorNumeric("RdYlBu", domain = census_data$map_data)
+                
+                leaflet(census_data) %>% 
+                        addProviderTiles(providers$CartoDB.Positron) %>%
+                        addPolygons(fillColor = ~pal(map_data),
+                                    color = "white",
+                                    weight = 1,
+                                    opacity = 1,
+                                    fillOpacity = 0.65) %>%
+                        addLegend("bottomright", pal = pal, values = ~map_data,
+                                  title = "Population",
+                                  labFormat = labelFormat(suffix = "%"),
+                                  opacity = 1
+                        )
+                
+                # # generate bins based on input$bins from ui.R
+                # x    <- faithful[, 2] 
+                # bins <- seq(min(x), max(x), length.out = input$bins + 1)
+                # 
+                # # draw the histogram with the specified number of bins
+                # hist(x, breaks = bins, col = 'darkgray', border = 'white')
         })
 }
