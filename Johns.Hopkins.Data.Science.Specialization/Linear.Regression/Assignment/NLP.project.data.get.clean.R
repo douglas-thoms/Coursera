e368497fd525851dda9452cd8cf2175569bd71f7
douglas-thoms <douglas.thoms@gmail.com>
2019-10-15 10:58:07 -0400

    starting NLP project

---
+++ Johns.Hopkins.Data.Science.Specialization/Linear.Regression/Assignment/NLP.project.data.get.clean.R
@@ -0,0 +1,35 @@
+##----------------------------------------------------------------------------
+##----------------------------------------------------------------------------
+##
+##  File name:  NLP.project.data.get.clean
+##  Date:       15OCT2019
+##
+##  Script to get and clean data
+##  
+##
+##----------------------------------------------------------------------------
+##----------------------------------------------------------------------------
+
+##----------------------------------------------------------------------------
+## Background
+##----------------------------------------------------------------------------
+
+##----------------------------------------------------------------------------
+## Libraries and system
+##----------------------------------------------------------------------------
+
+session.info.list <- sessionInfo()
+home.directory <- getwd()
+data.directory <- paste(home.directory,"data", sep = "/")
+dir.create(data.directory)
+
+##----------------------------------------------------------------------------
+## Acquiredata
+##---------------------------------------------------------------------------
+
+#training.data.loc <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
+
+#set up if loop to check for file
+#set up download date for file
+training.data.file <- download.file(training.data.loc, paste(data.directory,"Coursera-SwiftKey.zip",                                                          sep = "/"))
+                                                             
