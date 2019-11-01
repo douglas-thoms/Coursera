53ab2e62f547a357c56b8a0fc8d4373713f29c6c
douglas-thoms <douglas.thoms@gmail.com>
2019-11-01 08:26:39 -0400

    model research

--- Johns.Hopkins.Data.Science.Specialization/NLP.project/R.Project/scrap4.R
+++ Johns.Hopkins.Data.Science.Specialization/NLP.project/R.Project/scrap4.R
@@ -1,29 +1,57 @@
 library(dplyr)
 library(quanteda)
 
+#input string of words
+sentence <- "last year"
+#count number of words in sentence
+num.words <- sapply(strsplit(sentence, " "),length)
+#add in underscore
+sentence.under <- gsub(" ", "_", sentence)
+#get n-1gram - end of sentence till underscore
+ngram.one.smaller <- gsub( "_{1}.[^_]*$","",sentence.under)
+
+dict1 <- dictionary(list(search = paste(sentence.under,"*",sep="")))
+dict2 <- dictionary(paste(ngram.one.smaller,"*",sep=""))
+
+
+
 #use stupid back off model
 #alpha = 0.4
-#need to calculate unigram total, not total of bigram
+#need to calculate of n-1gram total, not total of bigram
 
 #set up small test sample
-df <-   bigram.dfm %>%
+df <-   trigram.dfm %>%
         dfm_subset(bigram.dfm[1:300]) %>%
-        #step, summarize bigram features into count
-        convert(to = "data.frame") %>%
-        select(-document) %>%
-        summarise_all(sum) %>%
-        select(matches("^last_*.")) %>%
-        t()
-        
+        dfm_select(trigram.dfm, dict1,valuetype = "glob")
+         # #step, summarize bigram features into count
+         # convert(to = "data.frame") %>%
+         # select(-document) %>%
+         # summarise_all(sum) %>%
+         # select(contains(sentence.under)) %>%
+         # t()
+
 df <- data.frame(bigram = row.names(df), df)
 
-df <- df %>%
-        rename(frequency = df) %>%
-        mutate(total = sum(frequency)) %>%
-        mutate(percent = frequency/total) %>%
-        arrange(desc(percent))
+# df1 <-  unigram.dfm %>%
+#         dfm_subset(unigram.dfm[1:300]) %>%
+#         dfm_lookup(dict2)
+#         #step, summarize bigram features into count
+#         convert(to = "data.frame")# %>%
+       # select(-document) # %>%
+       # summarise_all(sum) # %>%
+       # select(contains(ngram.one.smaller)) #%>%
+       # t()
 
-result <- df[1,1]
+#df1 <- data.frame(unigram = row.names(df1), df1)
+
+# 
+# df <- df %>%
+#         rename(frequency = df) %>%
+#         mutate(total = sum(frequency)) %>%
+#         mutate(percent = frequency/total) %>%
+#         arrange(desc(percent))
+# 
+# result <- df[1,1]
 #need to take out word    
         
 
